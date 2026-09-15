8838ad9ba51d9c7a0d7228f02384eb6bbbd2864a
volbix.admin <volbix.admin@gmail.com>
2026-09-15 11:29:59 +0530

    Update calculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import AppRoutes from "@/routes";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <div className="flex min-h-screen flex-col">
-        <Header />
+    return (
+        <HashRouter>
+            <div className="flex min-h-screen flex-col">
+                <Header />
 
-        <div className="flex-1">
-          <AppRoutes />
-        </div>
+                <div className="flex-1">
+                    <AppRoutes />
+                </div>
 
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+                <Footer />
+            </div>
+        </HashRouter>
+    );
 }
